54a6ab49bd4db7f2b30cd7dc60bb0bca8f4885c5
zhangmao <zhangmao@boya-triz.com>
2019-08-06 13:57:26 +0800

    初始化代码

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential', 
+    'standard'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'generator-star-spacing': 'off',
+    'semi': ['off', 'always'],    
+    'quotes': [1, 'double'],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
